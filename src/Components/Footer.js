b3d608bc6ae9ade71524ae896eb0b3a78523f68c
Rafael Escribano <r.escribano1@gmail.com>
2021-03-13 09:09:08 -0500

    style: Ensure color Scheme throughout. Update content for About, Portfolio, and Resume Components.
    Fixed some areas that were not display color scheme. Updated resumeData.json to include more background information about portfolio, resume info, and skills.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,34 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Footer extends Component {
-  render() {
 
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
+    render() {
+
+        if (this.props.data) {
+            var networks = this.props.data.social.map(function (network) {
+                return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+            })
+        }
+
+        return (
+            <footer>
+
+                <div className="row">
+                    <div className="twelve columns">
+                        <ul className="social-links">
+                            {networks}
+                        </ul>
+
+                        <ul className="copyright">
+                            {/*todo: Figure out how to dynamically populate copyrightText with year using function*/}
+                            <li>&copy; Copyright <span className="copyrightText">20xx</span> Escribano</li>
+                            <li>Design by <a title="Me" href="#">Ship of Theseus</a></li>
+                        </ul>
+
+                    </div>
+                    <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i
+                        className="icon-up-open"></i></a></div>
+                </div>
+            </footer>
+        );
     }
-
-    return (
-      <footer>
-
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
-
-           <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
-
-        </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
-    );
-  }
 }
 
 export default Footer;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
 
                     <div className="banner-text">
                         <h1 className="responsive-headline">{name}</h1>
-                        <h3>{description}.</h3>
+                        <h3>{description}:</h3>
                         {/*<hr/>*/}
                         {/*<ul className="social">*/}
                         {/*    <a href={project} className="button btn project-btn"><i*/}
