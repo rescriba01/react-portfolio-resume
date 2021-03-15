d667445caa1b0b7a06fdf3100969d7f0815a46a0
Rafael Escribano <r.escribano1@gmail.com>
2021-03-15 12:57:57 -0400

    style: Adjust flex items in Portfolio
    Adjusted the flex portfolio items so they are aligned in the center of their container since there are no more than 2 currently. Also updated the class to account for mobile viewports.

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -34,7 +34,7 @@ class Portfolio extends Component {
 
             <h1>Check Out Some of My Works.</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div id="portfolio-wrapper" className="bgrid-halves s-bgrid-quarters cf">
               {projects}
             </div>
           </div>
